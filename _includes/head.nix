1a61698fd6b0b34c2f5d7f14c33c40c082d34132
Oliver Kovacs <oliver@oliverkovacs.dev>
2026-07-25 15:42:30 +0200

    add title

--- _includes/head.nix
+++ _includes/head.nix
@@ -1,11 +1,11 @@
 ({ page, ... }:
 ''
 <head>
+    <title>${ page.title }</title>
+
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!--{%- seo -%}
-    {%- feed_meta -%}-->
 
     <link rel="icon" type="image/x-icon" href="/assets/img/favicon.png">
     <link rel="stylesheet" href="/assets/css/vars.css">
