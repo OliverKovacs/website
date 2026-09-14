93d5cd3fca1bd3b5353ded93e46bf2e4e4c8dcd1
Oliver Kovacs <oliver@oliverkovacs.dev>
2026-09-14 21:44:00 +0200

    publish ti

---
+++ ti.html.nix
@@ -0,0 +1,46 @@
+{ layouts, ... }:
+let
+    page = {
+        title = "Theoretische Informatik";
+        author = "Olivér Kovács";
+        noPrevNext = true;
+        noKatex = true;
+        noCode = true;
+    };
+in
+layouts.withPage.post page
+''
+<p>Hallo, ich bin euer TA für Theoretische Informatik!</p>
+
+<p>
+<b>Zeit</b>: Do 16-18 Uhr<br>
+<b>Raum</b>: <a href="https://ethz.ch/de/utils/location.html?building=HG&floor=F&room=26.5&lang=de" target="_blank">HG F 26.5</a>
+<!--<a href="https://ethz.ch/staffnet/de/service/raeume-gebaeude/rauminfo/raumdetails.html?building=HG&floor=F&room=26.5&lang=de" target="_blank">»</a>-->
+</p>
+
+<p>Fragen gerne auf <a href="tel:+436802258866">WhatsApp</a> oder per
+<a href="mailto:okovacs@ethz.ch">Email</a> stellen.</p>
+
+<h2 id="links">
+    Links <a href="#links">#</a>
+</h2>
+
+<p>[ <a href="https://www.vvz.ethz.ch/Vorlesungsverzeichnis/lerneinheit.view?lerneinheitId=204552&semkez=2026W" target="_blank">VVZ</a>,
+<a href="https://moodle-app2.let.ethz.ch/course/view.php?id=29077" target="_blank">Moodle</a>,
+<a href="https://video.ethz.ch/lectures/d-infk/2026/autumn/252-0057-00L" target="_blank">Videos</a>,
+<a href="https://link.springer.com/book/10.1007/978-3-658-06433-4" target="_blank">Buch</a>,
+<a href="https://exams.vis.ethz.ch/category/TheoretischeInformatik" target="_blank">Exams</a>,
+<a href="https://exams.vis.ethz.ch/exams/3bdetmew.pdf" target="_blank">Comsol</a> ]</p>
+
+<h2 id="material">
+    Material <a href="#material">#</a>
+</h2>
+
+<!--
+<h3 class="plain">Woche 1</h3>
+<ul class="dash">
+<li>Folien [ <a href="TBD">pdf</a>, <a href="TBD">tex</a> ]</li>
+<li>Aufgaben [ <a href="TBD">pdf</a>, <a href="TBD">tex</a> ]</li>
+</ul>
+-->
+''
